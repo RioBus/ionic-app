920c49c16adec39def0d49fc3f7fe6232b40fcc7
Fred Souza <fredericoamsouza@gmail.com>
2016-04-13 01:14:54 -0300

    Adding the map to the view

--- app/pages/home/page.ts
+++ app/pages/home/page.ts
@@ -1,11 +1,28 @@
 'use strict';
-import { Page } from 'ionic-angular';
+declare var plugin: any;
+import { Page, Platform } from 'ionic-angular';
 
 @Page({
   templateUrl: 'build/pages/home/template.html',
 })
 export class HomePage {
-  constructor() {
-    // no-op
-  }
+    
+    private static canvas: any;
+    private static map: any;
+    private platform: Platform;
+    
+    constructor(platform: Platform) {
+        this.platform = platform;
+        this.initializePage();
+    }
+    
+    private initializePage(): void {
+        this.platform.ready().then(() => {
+            let div: Element = document.getElementById("map_canvas");
+            let map: Element = plugin.google.maps.Map.getMap(div);
+            map.addEventListener(plugin.google.maps.event.MAP_READY, () => {
+                console.log("Map ready");
+            });
+        });
+    }
 }
