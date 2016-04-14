fa1c805e7b72a37e7d59003ab3222349d838857a
Fred Souza <fredericoamsouza@gmail.com>
2016-04-14 14:15:34 -0300

    Componentizing the map

---
+++ app/components/maps/controller.ts
@@ -0,0 +1,36 @@
+'use strict';
+declare var plugin: any;
+
+import { Modal, NavController, Platform } from 'ionic-angular';
+import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'google-maps',
+    templateUrl: 'build/components/maps/template.html',
+    inputs: [],
+    directives: [],
+})
+
+export class GoogleMaps {
+    
+    private nav: NavController;
+    private platform: Platform;
+    private canvas: HTMLElement;
+    private map: any;
+    
+    constructor(platform: Platform, nav: NavController) {
+        this.platform = platform;
+        this.nav = nav;
+        this.initializePage();
+    }
+    
+    private initializePage(): void {
+        this.platform.ready().then(() => {
+            this.canvas = document.getElementById("map_canvas");
+            this.map = plugin.google.maps.Map.getMap(this.canvas);
+            this.map.addEventListener(plugin.google.maps.event.MAP_READY, () => {
+                console.log("Map ready");
+            });
+        });
+    }
+}
--- app/pages/home/page.ts
+++ app/pages/home/page.ts
@@ -1,10 +1,12 @@
 'use strict';
 declare var plugin: any;
 import { Page, Platform, NavController } from 'ionic-angular';
+import { GoogleMaps } from '../../components/maps/controller';
 import { SearchPage } from '../search/page';
 
 @Page({
   templateUrl: 'build/pages/home/template.html',
+  directives: [GoogleMaps]
 })
 export class HomePage {
     
@@ -16,18 +18,18 @@ export class HomePage {
     constructor(platform: Platform, nav: NavController) {
         this.platform = platform;
         this.nav = nav;
-        this.initializePage();
+        // this.initializePage();
     }
     
-    private initializePage(): void {
-        this.platform.ready().then(() => {
-            this.canvas = document.getElementById("map_canvas");
-            this.map = plugin.google.maps.Map.getMap(this.canvas);
-            this.map.addEventListener(plugin.google.maps.event.MAP_READY, () => {
-                console.log("Map ready");
-            });
-        });
-    }
+    // private initializePage(): void {
+    //     this.platform.ready().then(() => {
+    //         this.canvas = document.getElementById("map_canvas");
+    //         this.map = plugin.google.maps.Map.getMap(this.canvas);
+    //         this.map.addEventListener(plugin.google.maps.event.MAP_READY, () => {
+    //             console.log("Map ready");
+    //         });
+    //     });
+    // }
     
     public openSearch(): void {
         this.nav.push(SearchPage);
