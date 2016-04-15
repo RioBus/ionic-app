d6d6b056dd6f4544797b36662d7b702bb6d7adc2
Fred Souza <fredericoamsouza@gmail.com>
2016-04-14 22:32:21 -0300

    Creating SearchService class

---
+++ app/services/search.ts
@@ -0,0 +1,30 @@
+'use strict';
+import { API_ENDPOINT } from '../const';
+import { Bus } from '../models/bus';
+import { Injectable } from 'angular2/core';
+import { Http } from 'angular2/http';
+
+@Injectable()
+export class SearchService {
+    
+    private http: Http;
+    
+    constructor(http: Http) {
+        this.http = http;
+    }
+
+    private processBuses(obj: any[]): Bus[] {
+        let result: Bus[] = [];
+        obj.forEach((data) => {
+            result.push(new Bus(data.line, data.order, data.speed, data.direction, data.latitude, data.longitude, data.sense, data.timeStamp));
+        });
+        return result;
+    }
+    
+    public getBuses(query: string): Promise<Bus[]> {
+        return new Promise<Bus[]>(resolve => {
+            let url: string = `${API_ENDPOINT}/v3/search/${query}`;
+            this.http.get(url).subscribe(data => resolve( this.processBuses( data.json() ) ) );
+        });
+    }
+}
