77057ca899b2f99e35500a80d456f261e3b29418
Fred Souza <fredericoamsouza@gmail.com>
2016-04-14 22:33:44 -0300

    Creating Bus models

---
+++ app/models/bus.ts
@@ -0,0 +1,56 @@
+'use strict';
+
+export class Bus {
+    
+    private line: string;
+    private order: string;
+    private speed: number;
+    private latitude: number;
+    private longitude: number;
+    private timestamp: Date;
+    private direction: string;
+    private directionDegrees: number;
+    
+    public get Line(): string {
+        return this.line;
+    }
+    
+    public get Order(): string {
+        return this.order;
+    }
+    
+    public get Speed(): number {
+        return this.speed;
+    }
+    
+    public get DirectionDegrees(): number {
+        return this.directionDegrees;
+    }
+    
+    public get Latitude(): number {
+        return this.latitude;
+    }
+    
+    public get Longitude(): number {
+        return this.longitude;
+    }
+    
+    public get Direction(): string {
+        return this.direction;
+    }
+    
+    public get Timestamp(): Date {
+        return this.timestamp;
+    }
+    
+    constructor(line: string, order: string, speed: number, directionDegrees: number, latitude: number, longitude: number, direction: string, timestamp: Date) {
+        this.line = line;
+        this.order = order;
+        this.speed = speed;
+        this.directionDegrees = directionDegrees;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.direction = direction;
+        this.timestamp = timestamp;
+    }
+}
