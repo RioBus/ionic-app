51fdee6f219c033e53ee4f236075149bece97174
Fred Souza <fredericoamsouza@gmail.com>
2016-04-17 01:11:03 -0300

    Retrieving the searched buses in the map page

--- app/pages/map/page.ts
+++ app/pages/map/page.ts
@@ -2,7 +2,9 @@
 declare var plugin: any;
 import { Page, Platform, NavController, NavParams } from 'ionic-angular';
 import { GoogleMaps } from '../../components/maps/controller';
-import { SearchPage } from '../search/page';
+import { MapManager } from '../../managers/map';
+import { SearchService } from '../../services/search';
+import { Bus } from '../../models/bus';
 
 @Page({
   templateUrl: 'build/pages/map/template.html',
@@ -11,20 +13,29 @@ import { SearchPage } from '../search/page';
 export class MapPage {
     
     private canvas: Element;
-    private map: any;
+    private manager: MapManager;
     private platform: Platform;
     private nav: NavController;
     private params: NavParams;
+    private service: SearchService;
     private title: string = 'Rio Bus';
     
     public get Title(): string {
         return this.title;
     }
     
-    constructor(platform: Platform, nav: NavController, params: NavParams) {
+    constructor(platform: Platform, nav: NavController, params: NavParams, service: SearchService) {
         this.platform = platform;
         this.params = params;
         this.nav = nav;
+        this.service = service;
+        this.manager = MapManager.getInstance();
         this.title = this.params.data.query;
+    }
+    
+    private onPageLoaded(): void {
+        this.service.getBuses(this.title).then((buses: Bus[]) => {
+            console.log(`Found ${buses.length} buses.`);
+        });
     }
 }
