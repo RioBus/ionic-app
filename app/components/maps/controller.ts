6577982d45e37f7f6cfedd6e931d70642e007342
Fred Souza <fredericoamsouza@gmail.com>
2016-04-17 12:17:22 -0300

    Inserting markers on the map

--- app/components/maps/controller.ts
+++ app/components/maps/controller.ts
@@ -16,9 +16,10 @@ export class GoogleMaps implements OnChanges {
     
     private nav: NavController;
     private platform: Platform;
-    private canvas: HTMLElement;
     private map: any;
     private timer: any;
+    private markers: Bus[];
+    private markerList: any[] = [];
     
     constructor(platform: Platform, nav: NavController) {
         this.platform = platform;
@@ -26,21 +27,63 @@ export class GoogleMaps implements OnChanges {
     }
     
     private ngOnInit(): void {
-        this.platform.ready().then(() => { 
-            this.canvas = document.getElementById("map_canvas");
-            this.map = plugin.google.maps.Map.getMap(this.canvas);
+        this.platform.ready().then(() => {
+            this.map = plugin.google.maps.Map.getMap(document.getElementById("map_canvas"));
             this.map.addEventListener(plugin.google.maps.event.MAP_READY, () => {
                 console.log("Map ready");
             });
         });
     }
     
-    ngOnChanges(changes: any) {
-        if(changes.markers) {
-            if(changes.markers.previousValue)
-                console.log(`Previous markers: ${changes.markers.previousValue.length}`);
-            if(changes.markers.currentValue)
-                console.log(`Current markers: ${changes.markers.currentValue.length}`);
+    private ngOnDestroy(): void {
+        console.log("Leaving map.");
+        this.removeMarkers();
+    }
+    
+    private isArray(value: any): boolean {
+        return value instanceof Array;
+    }
+    
+    public ngOnChanges(changes: any) {
+        if(!changes.markers.previousValue && this.isArray(changes.markers.currentValue)) {
+            // Just loaded the map view
+            if(this.markerList.length>0) this.removeMarkers();
+            this.insertNewMarkers(changes.markers.currentValue);
+        } else if(this.isArray(changes.markers.previousValue) && this.isArray(changes.markers.currentValue)) {
+            // Received new data
+                if(JSON.stringify(changes.markers.previousValue)!==JSON.stringify(changes.markers.currentValue)) {
+                    console.log("The data had an update.");
+                } else console.log("Still no changes");
+        }
+    }
+    
+    private removeMarkers(): void {
+        this.markerList.forEach((value, index) => {
+            value.remove();
+            delete this.markers[index];
+        });
+    }
+    
+    private addMarker(marker: any): void {
+        this.markerList.push(marker);
+    }
+    
+    private insertNewMarkers(buses: Bus[]): void {
+        buses.forEach((bus: Bus) => {
+            let location = new plugin.google.maps.LatLng(bus.Latitude, bus.Longitude);
+            this.map.addMarker({
+                position: location,
+                title: bus.Line
+            }, (marker) => { this.addMarker(marker); });
+        }, this);
+    }
+    
+    private UpdateMarkers(buses: Bus[], previous: Bus[]): void {
+        if(!previous && this.markers.length>0) {
+            for(let i=0; i<this.markers.length; i++) {
+                this.markerList[i].remove();
+                delete this.markers[i];
+            }
         }
     }
 }
