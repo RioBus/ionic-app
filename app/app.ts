47238cecaf61a3d7e9497388869d425bb5bb5586
Fred Souza <fredericoamsouza@gmail.com>
2016-04-21 18:22:23 -0300

    Refactoring Application class

--- app/app.ts
+++ app/app.ts
@@ -7,20 +7,21 @@ import { App, IonicApp, Platform }     from 'ionic-angular';
 import { AboutPage }                   from './pages/about/page';
 import { FavoritesPage }               from './pages/favorites/page';
 import { HistoryPage }                 from './pages/history/page';
-import { MapPage }                     from './pages/map/page';
 import { SearchPage }                  from './pages/search/page';
 import { ItineraryService }            from './services/itinerary';
 import { SearchService }               from './services/search';
 
+// App decorator: http://ionicframework.com/docs/v2/api/decorators/App/
 @App({
   templateUrl: 'build/app.html',
-  config: {}, // http://ionicframework.com/docs/v2/api/config/Config/
-  providers: [ItineraryService, SearchService]
+  providers: [ItineraryService, SearchService],
+  prodMode: true,
+  config: {},
 })
 export class Application {
 
   private rootPage: Type;
-  private pages: Array<{title: string, icon: string, component?: Type, link?: string}>;
+  private pages: Array<{ title: string, icon: string, component?: Type, link?: string }>;
   private app: IonicApp;
   private platform: Platform;
   private backPressed: boolean;
@@ -28,8 +29,7 @@ export class Application {
   constructor(app: IonicApp, platform: Platform) {
     this.app = app;
     this.platform = platform;
-    this.rootPage = SearchPage; //HomePage;
-    this.initializeApp();
+    this.rootPage = SearchPage;
 
     // set our app's pages
     this.pages = [
@@ -39,37 +39,39 @@ export class Application {
       { title: 'Avalie o app', icon: 'appstore', link: URL_PLAY_STORE },
       { title: 'Curta no Facebook', icon: 'thumbs-up', link: URL_FB_PAGE }
     ];
+
+    this.initializeApp();
   }
 
-  public openPage(page: {title: string, icon: string, component?: Type, link?: string}): void {
-    if(page.component) {
+  public openPage(page: { title: string, icon: string, component?: Type, link?: string }): void {
+    if (page.component) {
       // close the menu when clicking a link from the menu
       this.app.getComponent('leftMenu').close();
       // navigate to the new page if it is not the current page
       this.app.getComponent('nav').push(page.component);
-    } else if(page.link) {
+    } else if (page.link) {
       window.open(page.link, '_system');
     }
   }
 
   private initializeApp(): void {
-    this.platform.ready().then(() => {
-      this.registerBackButton();
-    });
+    this.platform.ready().then(() => this.onPlatformReady());
   }
-  
-  private registerBackButton(): void {
-      document.addEventListener('backbutton', () => {
-        if (this.app.getComponent('nav').canGoBack()) {
-          this.app.getComponent('nav').pop();
-          return;
-        }
-        if(!this.backPressed) {
-          this.backPressed = true;
-          setTimeout(() => this.backPressed = false, 2000);
-          return;
-        }
-        navigator.app.exitApp();
-      }, false);
+
+  private onPlatformReady(): void {
+    document.addEventListener('backbutton', () => this.handleBackButton(), false);
+  }
+
+  private handleBackButton(): void {
+    if (this.app.getComponent('nav').canGoBack()) {
+      this.app.getComponent('nav').pop();
+      return;
+    }
+    if (!this.backPressed) {
+      this.backPressed = true;
+      setTimeout(() => this.backPressed = false, 2000);
+      return;
+    }
+    navigator.app.exitApp();
   }
 }
