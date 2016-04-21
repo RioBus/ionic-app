ee5658b1749923fb79d00a1997513851cbc39a88
Fred Souza <fredericoamsouza@gmail.com>
2016-04-21 02:08:31 -0300

    Linting file

--- app/components/maps/controller.ts
+++ app/components/maps/controller.ts
@@ -1,19 +1,12 @@
 'use strict';
 declare var plugin: any, google: any;
 
-import { SERVER_ADDR } from '../../const';
-import { Modal, NavController, Platform, Button, Icon } from 'ionic-angular';
+import { NavController, Platform } from 'ionic-angular';
 import { MapSnackbar } from '../map-snackbar/controller';
 import { Component, OnChanges } from 'angular2/core';
 import { Bus } from '../../models/bus';
 import { Line } from '../../models/itinerary';
 import { MarkerController } from './marker';
-
-let markerIcons: any = {
-    good: 'www/img/bus_green.png',
-    average: 'www/img/bus_yellow.png',
-    bad: 'www/img/bus_red.png'
-};
 
 @Component({
     selector: 'google-maps',
@@ -22,93 +15,91 @@ let markerIcons: any = {
     directives: [MapSnackbar],
 })
 export class GoogleMaps implements OnChanges {
-    
-    private static markerList: any = {};
+
     private nav: NavController;
     private platform: Platform;
     private map: any;
-    private timer: any;
     private markers: Bus[];
     private line: Line;
     private mcontrol: MarkerController;
     private swap: boolean = false;
     private static instance: GoogleMaps;
-    
+
     public get Line(): Line {
         return this.line;
     }
-    
+
     constructor(platform: Platform, nav: NavController) {
         this.platform = platform;
         this.nav = nav;
         GoogleMaps.instance = this;
     }
-    
+
     private isArray(value: any): boolean {
         return value instanceof Array;
     }
-    
+
     private ngOnInit(): void {
         this.platform.ready().then(() => { this.onPlatformRedy(); });
     }
-    
+
     private ngOnDestroy(): void {
         this.removeMarkers();
     }
-    
+
     public ngOnChanges(changes: any): void {
         this.checkMarkerChanges(changes.markers);
     }
-    
+
     private onPlatformRedy(): void {
         // Map centered in RJ
-        this.map = plugin.google.maps.Map.getMap(document.getElementById("map_canvas"), {
+        this.map = plugin.google.maps.Map.getMap(document.getElementById('map_canvas'), {
             mapType: plugin.google.maps.MapTypeId.ROADMAP,
             controls: { compass: true, myLocationButton: true, indoorPicker: false, zoom: false },
-            camera: { latLng: new plugin.google.maps.LatLng(-22.9083, -43.1964), zoom: 12 }
+            camera: { latLng: new plugin.google.maps.LatLng(-22.9083, -43.1964), zoom: 12 },
         });
         this.mcontrol = new MarkerController(this.map);
         this.map.addEventListener(plugin.google.maps.event.MAP_READY, () => { this.onMapReady(); });
     }
-    
+
     private onMapReady(): void {
-        console.log("Map ready");
+        console.log('Map ready');
     }
-    
+
     public onSwapDirection(): boolean {
-        let This: GoogleMaps = GoogleMaps.instance;
-        if(This.Line.Description !== 'desconhecido') {
-            This.swap = !This.swap;
-            This.removeMarkers();
-            This.updateMarkers(This.markers);
+        let self: GoogleMaps = GoogleMaps.instance;
+        if (self.Line.Description !== 'desconhecido') {
+            self.swap = !self.swap;
+            self.removeMarkers();
+            self.updateMarkers(self.markers);
             return true;
         }
         return false;
     }
-    
+
     private updateMarkers(current: Bus[]): void {
-        this.filterBuses(current).forEach( bus => {
+        this.filterBuses(current).forEach(bus => {
             this.mcontrol.setMarker(bus);
         });
     }
-    
+
     private removeMarkers(): void {
         this.mcontrol.removeMarkers();
     }
-    
+
     private filterBuses(buses: Bus[]): Bus[] {
-        if(!this.swap) return buses.filter( bus => bus.Direction === this.line.Description );
-        else return buses.filter( bus => bus.Direction !== this.line.Description );
+        if (!this.swap) return buses.filter(bus => bus.Direction === this.line.Description);
+        else return buses.filter(bus => bus.Direction !== this.line.Description);
     }
-    
+
     private checkMarkerChanges(markers: any): void {
-        if(!markers.previousValue && this.isArray(markers.currentValue)) {
+        if (!markers.previousValue && this.isArray(markers.currentValue)) {
             // Just loaded the map view
             this.removeMarkers();
             this.updateMarkers(markers.currentValue);
-        } else if(this.isArray(markers.previousValue) && this.isArray(markers.currentValue)) {
+        } else if (this.isArray(markers.previousValue) && this.isArray(markers.currentValue)) {
             // Received new data
-            if(JSON.stringify(markers.previousValue)!==JSON.stringify(markers.currentValue))
+            if (JSON.stringify(markers.previousValue) !== JSON.stringify(markers.currentValue))
                 this.updateMarkers(markers.currentValue);
         }
     }
