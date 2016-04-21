f4830080e7d01db4b9eb7678baf7492488e44420
Fred Souza <fredericoamsouza@gmail.com>
2016-04-21 02:21:54 -0300

    Fixing issue: the Lines are not downloaded in the first run

--- app/pages/search/page.ts
+++ app/pages/search/page.ts
@@ -9,45 +9,45 @@ import { HistoryDAO } from '../../dao/history';
 import { LinesDAO } from '../../dao/lines';
 
 @Page({
-  templateUrl: 'build/pages/search/template.html',
+    templateUrl: 'build/pages/search/template.html',
 })
 export class SearchPage {
-    
+
     private itineraryService: ItineraryService;
     private nav: NavController;
-    
+
     private items: Line[] = [];
     private itemsBkp: Line[] = [];
     private favorites: Line[] = [];
     private histories: History[] = [];
-    
+
     private fdao: FavoritesDAO;
     private hdao: HistoryDAO;
     private ldao: LinesDAO;
-    
+
     private limit: number = 10;
     private skip: number = 0;
     public showSearchBox: boolean = false;
-    
+
     public get Items(): Line[] {
         return this.items;
     }
-    
+
     public get Histories(): History[] {
         return this.histories;
     }
-    
+
     public isFavorite(line: Line): boolean {
-        return this.favorites.some( fav => line.Line === fav.Line );
+        return this.favorites.some(fav => line.Line === fav.Line);
     }
-    
+
     public get searchBox(): any {
         let element: any = document.getElementById('searchbox');
-        if(element) element = element.getElementsByTagName('input');
-        if(element) element = element.item(0);
-        return (element)? element : null;
+        if (element) element = element.getElementsByTagName('input');
+        if (element) element = element.item(0);
+        return (element) ? element : null;
     }
-    
+
     public constructor(nav: NavController, itineraryService: ItineraryService) {
         this.nav = nav;
         this.itineraryService = itineraryService;
@@ -55,61 +55,61 @@ export class SearchPage {
         this.hdao = new HistoryDAO();
         this.ldao = new LinesDAO();
     }
-    
+
     public onPageLoaded(): void {
         this.loadRecents();
         this.loadFavorites();
     }
-    
+
     public onPageWillEnter(): void {
         document.getElementById('search-view').style.display = 'initial';
     }
-    
+
     public onPageWillLeave(): void {
         document.getElementById('search-view').style.display = 'none';
     }
-    
+
     public onClickOnStar(line: Line): void {
-        if(this.isFavorite(line)) this.fdao.remove(line).then( (response: boolean) => this.onUnstar(response, line) );
-        else this.fdao.save(line).then( (response: boolean) => this.onStar(response, line) );
+        if (this.isFavorite(line)) this.fdao.remove(line).then((response: boolean) => this.onUnstar(response, line));
+        else this.fdao.save(line).then((response: boolean) => this.onStar(response, line));
     }
-    
+
     private onStar(response: boolean, line: Line): void {
-        if(response) this.favorites.push(line);
+        if (response) this.favorites.push(line);
     }
-    
+
     private onUnstar(response: boolean, line: Line): void {
-        if(response) {
-            let index: number = this.favorites.findIndex( fav => fav.Line === line.Line );
+        if (response) {
+            let index: number = this.favorites.findIndex(fav => fav.Line === line.Line);
             this.favorites.splice(index, 1);
         }
     }
-    
+
     public find(line: Line): void {
         let history: History = new History(line, new Date());
-        this.hdao.save(history).then( saved => {
-            if(saved) console.log(`Saved ${line.Line} to history.`);
+        this.hdao.save(history).then(saved => {
+            if (saved) console.log(`Saved ${line.Line} to history.`);
             this.nav.push(MapPage, { line: line });
         });
     }
-    
+
     public findText(): void {
         let query: string = this.searchBox.value.replace('  ', ' ').replace(' , ', ',').replace(', ', ',').replace(' ,', ',');
         let line: Line = new Line(query, '');
         let history: History = new History(line, new Date());
-        this.hdao.save(history).then( saved => {
-            if(saved) console.log(`Saved ${line.Line} to history.`);
+        this.hdao.save(history).then(saved => {
+            if (saved) console.log(`Saved ${line.Line} to history.`);
             this.nav.push(MapPage, { query: query });
         });
     }
-    
+
     public filter(event: any): void {
         let query: string = this.searchBox.value;
-        if(query.length>0) {
-            if(this.itemsBkp.length===0) this.itemsBkp = this.items;
+        if (query.length > 0) {
+            if (this.itemsBkp.length === 0) this.itemsBkp = this.items;
             this.ldao.getAll().then(lines => {
                 this.items = lines.filter((value: Line, index: number, lines: Line[]): boolean => {
-                    return value.Line.toLowerCase().indexOf(query.toLowerCase())>-1 || value.Description.toLowerCase().indexOf(query.toLowerCase())>-1;
+                    return value.Line.toLowerCase().indexOf(query.toLowerCase()) > -1 || value.Description.toLowerCase().indexOf(query.toLowerCase()) > -1;
                 });
             });
         } else {
@@ -117,54 +117,53 @@ export class SearchPage {
             this.itemsBkp = [];
         }
     }
-    
+
     public showSearchBar(): void {
         this.showSearchBox = true;
-        setTimeout(() => this.searchBox.focus() , 1); 
+        setTimeout(() => this.searchBox.focus(), 1);
     }
-    
+
     public hideSearchBar(): void {
-        if(this.searchBox.value.length>0) {
+        if (this.searchBox.value.length > 0) {
             this.searchBox.value = '';
             this.filter(null);
         }
         this.showSearchBox = false;
     }
-    
+
     private loadRecents(): void {
-        this.hdao.getLimited(2).then( histories => this.histories = histories );
+        this.hdao.getLimited(2).then(histories => this.histories = histories);
     }
-    
+
     private loadFavorites(): void {
         this.fdao.getAll().then((lines: Line[]) => {
             this.favorites = lines;
             this.loadLines();
         });
     }
-    
+
     private loadLines(infiniteScroll?: any): void {
         this.ldao.getLimited(this.limit, this.skip).then(lines => {
-            if(lines.length>0) {
+            if (lines.length > 0) {
                 this.items = this.items.concat(lines);
                 this.skip += this.limit;
-                if(infiniteScroll) infiniteScroll.complete();
+                if (infiniteScroll) infiniteScroll.complete();
             }
             else this.downloadLines(infiniteScroll);
         });
     }
-    
+
     private downloadLines(infiniteScroll?: any): void {
-        if(this.searchBox) {
+        if (this.showSearchBox) {
             let query: string = this.searchBox.value.toString();
-            if(query.length>0 && infiniteScroll) infiniteScroll.complete();
-            
+            if (query.length > 0 && infiniteScroll) infiniteScroll.complete();
+
         } else this.itineraryService.getItineraries().then((lines: Line[]) => {
-            this.ldao.saveAll(lines).then( () => {
-                console.log(`Saved ${lines.length}.`);
+            this.ldao.saveAll(lines).then(() => {
                 let slice: Line[] = lines.splice(0, this.limit);
                 this.items = this.items.concat(slice);
                 this.skip += this.limit;
-                if(infiniteScroll) infiniteScroll.complete();
+                if (infiniteScroll) infiniteScroll.complete();
             });
         });
     }
