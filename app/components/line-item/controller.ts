f82395100fcca28b5287886e5ee90d42dc841eb4
Fred Souza <fredericoamsouza@gmail.com>
2016-04-22 00:50:19 -0300

    Saving Line to History memory when clicking in the line-item component

--- app/components/line-item/controller.ts
+++ app/components/line-item/controller.ts
@@ -3,7 +3,9 @@
 import { NavController } from 'ionic-angular';
 import { Component } from 'angular2/core';
 import { Line } from '../../models/itinerary';
+import { History } from '../../models/history';
 import { MapPage } from '../../pages/map/page';
+import { HistoryDAO } from '../../dao/history';
 
 @Component({
     selector: 'line-item',
@@ -14,12 +16,18 @@ export class LineItem {
 
     public line: Line;
     private nav: NavController;
+    private dao: HistoryDAO;
 
     public constructor(nav: NavController) {
         this.nav = nav;
+        this.dao = new HistoryDAO();
     }
 
     public onClick(): void {
-        this.nav.push(MapPage, { line: this.line });
+        let history: History = new History(this.line, new Date());
+        this.dao.save(history).then(saved => {
+            if (saved) console.log(`Saved ${this.line.Line} to history.`);
+            this.nav.push(MapPage, { line: this.line });
+        });
     }
 }
