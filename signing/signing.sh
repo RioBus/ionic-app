470b78548dec9a7dab02ce51e66803b061aad002
Fred Souza <fredericoamsouza@gmail.com>
2016-06-09 19:26:45 -0300

    Moving debug keystore to signing/

--- signing/signing.sh
+++ signing/signing.sh
@@ -3,5 +3,5 @@ echo "Configuring Android signing properties...";
 cp "$(pwd)/config/debug-signing.properties" "$(pwd)/platforms/android";
 cp "$(pwd)/config/release-signing.properties" "$(pwd)/platforms/android";
 cp "$(pwd)/config/debug.keystore" "$(pwd)/platforms/android";
-cp "$(pwd)/config/release.keystore" "$(pwd)/platforms/android";
+# cp "$(pwd)/config/release.keystore" "$(pwd)/platforms/android";
 echo "Finished.";
