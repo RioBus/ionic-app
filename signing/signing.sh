64026004d6b08c8b845cb98998ec57561df79cd4
Fred Souza <fredericoamsouza@gmail.com>
2016-06-10 01:06:00 -0300

    Adding platform before copying keys

--- signing/signing.sh
+++ signing/signing.sh
@@ -1,5 +1,6 @@
 # Copying the debug signing properties to Android project root
 echo "Configuring Android signing properties...";
+ionic platform add android
 cp "$(pwd)/signing/debug-signing.properties" "$(pwd)/platforms/android";
 cp "$(pwd)/signing/release-signing.properties" "$(pwd)/platforms/android";
 cp "$(pwd)/signing/debug.keystore" "$(pwd)/platforms/android";
