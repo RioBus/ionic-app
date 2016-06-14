de50f0d7c30637a9135d08c4f7976a69dd8836dd
Fred Souza <fredericoamsouza@gmail.com>
2016-06-14 01:18:10 -0300

    Documenting FavoriteButton component class

--- app/components/favorite-button/controller.ts
+++ app/components/favorite-button/controller.ts
@@ -1,10 +1,12 @@
-'use strict';
-
 import { Button, Icon } from 'ionic-angular';
 import { Component } from '@angular/core';
 import { Line } from '../../models/itinerary';
 import { FavoritesDAO } from '../../dao/favorites';
 
+/**
+ * Represents the <favorite-button> HTML component.
+ * @class {FavoriteButton}
+ */
 @Component({
     selector: 'favorite-button',
     templateUrl: 'build/components/favorite-button/template.html',
@@ -26,24 +28,44 @@ export class FavoriteButton {
         return this.favorite;
     }
 
+    /**
+     * It's part of Angular2 lifecycle. It runs when the view is initialized.
+     * @return {void}
+     */
     public ngOnInit(): void {
         this.favoriteCheck();
     }
 
+    /**
+     * Checks if the given line is favorite or not.
+     * @return {void}
+     */
     private favoriteCheck(): void {
         this.dao.getByLine(this.line.Line).then((line: Line) => this.favorite = !!line );
     }
 
+    /**
+     * Handles the event for when the user clicks over the star button
+     * @return {void}
+     */
     public onClick(): void {
         if (this.favorite) this.dao.remove(this.line).then((response: boolean) => this.onUnstar(response));
         else this.dao.save(this.line).then((response: boolean) => this.onStar(response));
     }
 
+    /**
+     * Event for when the user is trying to star a line
+     * @return {void}
+     */
     private onStar(response: boolean): void {
         if (response) this.favorite = true;
         else console.log(`Failed to star the line '${this.line.Line}'`);
     }
 
+    /**
+     * Event for when the user is trying to unstar a line
+     * @return {void}
+     */
     private onUnstar(response: boolean): void {
         if (response) this.favorite = false;
         else console.log(`Failed to unstar the line '${this.line.Line}'`);
