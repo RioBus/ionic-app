0039c00bba844d6d55fa2321edae3371d14c8680
Fred Souza <fredericoamsouza@gmail.com>
2016-06-14 01:21:05 -0300

    Documenting HistoryDropdownList component class

--- app/components/history-dropdown-list/controller.ts
+++ app/components/history-dropdown-list/controller.ts
@@ -1,10 +1,12 @@
-'use strict';
-
 import { Button, Icon, List, Item, NavController } from 'ionic-angular';
 import { Component } from '@angular/core';
 import { History } from '../../models/history';
 import { LineItem } from '../line-item/controller';
 
+/**
+ * Represents the <history-dropdown-list> HTML component.
+ * @class {HistoryDropdownList}
+ */
 @Component({
     selector: 'history-dropdown-list',
     templateUrl: 'build/components/history-dropdown-list/template.html',
@@ -16,16 +18,19 @@ export class HistoryDropdownList {
     public title: string;
     public items: History[];
     private show: boolean = false;
-    private nav: NavController;
 
-    public constructor(nav: NavController) {
-        this.nav = nav;
-    }
-
+    /**
+     * Toggles a group's cascade visualization on/off
+     * @return {void}
+     */
     public toggleItem(): void {
         this.show = !this.show;
     }
 
+    /**
+     * Answers wether it should display the history group or not.
+     * @return {boolean}
+     */
     public presentGroup(): boolean {
         return this.show;
     }
