2a93dd829106500d839df70f4927d15c0317ed8a
Fred Souza <fredericoamsouza@gmail.com>
2016-06-14 18:26:22 -0300

    Setting up PT menu strings

--- app/strings/pt-BR.ts
+++ app/strings/pt-BR.ts
@@ -1,4 +1,11 @@
 export const PAGE_HOME_TITLE: string = 'Ionic';
 export const PAGE_HOME_CONTENT_TITLE: string = 'Alô mundo!';
-export const MENU_TITLE: string = 'Menu';
-export const MENU_OPTION_HOME: string = 'Início';
+
+// Sidemenu
+export const MENU_TITLE: string = 'Rio Bus';
+export const MENU_OPTION_SEARCH: string = 'Buscar';
+export const MENU_OPTION_FAVORITES: string = 'Favoritos';
+export const MENU_OPTION_HISTORY: string = 'Histórico';
+export const MENU_OPTION_ABOUT: string = 'Sobre';
+export const MENU_OPTION_RATE: string = 'Avalie o app';
+export const MENU_OPTION_LIKE: string = 'Curta no Facebook';
