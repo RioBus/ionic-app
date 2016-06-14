66ef85e1ec865713508b578265f2c932e5d20954
Fred Souza <fredericoamsouza@gmail.com>
2016-06-14 18:26:12 -0300

    Setting up EN menu strings

--- app/strings/en-US.ts
+++ app/strings/en-US.ts
@@ -1,4 +1,11 @@
 export const PAGE_HOME_TITLE: string = 'Ionic';
 export const PAGE_HOME_CONTENT_TITLE: string = 'Hello world!';
-export const MENU_TITLE: string = 'Menu';
-export const MENU_OPTION_HOME: string = 'Home';
+
+// Sidemenu
+export const MENU_TITLE: string = 'Rio Bus';
+export const MENU_OPTION_SEARCH: string = 'Search';
+export const MENU_OPTION_FAVORITES: string = 'Favorites';
+export const MENU_OPTION_HISTORY: string = 'History';
+export const MENU_OPTION_ABOUT: string = 'About';
+export const MENU_OPTION_RATE: string = 'Rate the app';
+export const MENU_OPTION_LIKE: string = 'Like Facebook page';
