48b5ddccb242cc2d2cbf5d916276b957ebb6014e
Fred Souza <fredericoamsouza@gmail.com>
2016-06-14 20:33:52 -0300

    Documenting LineManager class

--- app/managers/line.ts
+++ app/managers/line.ts
@@ -1,10 +1,15 @@
-'use strict';
-
 import { ItineraryService } from '../services/itinerary';
 import { LinesDAO } from '../dao/lines';
 import { Line } from '../models/itinerary';
 import { Injectable } from '@angular/core';
 
+/**
+ * LineManager class is responsible for handling the access to the
+ * Line data you need to retrieve. It abstracts all the logic operations
+ * needed to guarantee that you will receive your data when requested.
+ *
+ * @class {LineManager}
+ */
 @Injectable()
 export class LineManager {
 
@@ -16,6 +21,11 @@ export class LineManager {
         this.dao = new LinesDAO();
     }
 
+    /**
+     * @private
+     * Downloads all the Lines from the external API.
+     * @return {Promise<Line[]>}
+     */
     private downloadLines(): Promise<Line[]> {
         return new Promise<Line[]>(resolve => {
             this.service.getItineraries().then((lines: Line[]) => {
@@ -26,22 +36,16 @@ export class LineManager {
         });
     }
 
+    /**
+     * Retrieves all known Line instances from the repository.
+     * @return {Line[]}
+     */
     public getAll(): Promise<Line[]> {
         return new Promise<Line[]>(resolve => {
             this.dao.getAll().then(daoLines => {
-                if (daoLines.length > 0)
-                    resolve(daoLines);
-                else
-                    this.downloadLines().then(downloadedLines => resolve(downloadedLines));
-            });
-        });
-    }
-
-    public getSlice(limit: number, skip: number): Promise<Line[]> {
-        return new Promise<Line[]>(resolve => {
-            this.dao.getLimited(limit, skip).then( lines => {
-                if (lines.length > 0) resolve(lines);
-                else this.downloadLines().then( downloadedLines => resolve(downloadedLines.splice(skip, limit)) );
+                if (daoLines.length > 0) resolve(daoLines);
+                else this.downloadLines()
+                    .then(downloadedLines => resolve(downloadedLines));
             });
         });
     }
