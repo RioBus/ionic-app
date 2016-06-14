eceda0d08ab9fefe662c701d88059fb1be44c30b
Fred Souza <fredericoamsouza@gmail.com>
2016-06-14 01:27:03 -0300

    Documenting MapSnackbar component class

--- app/components/map-snackbar/controller.ts
+++ app/components/map-snackbar/controller.ts
@@ -1,10 +1,12 @@
-'use strict';
-
 import { Button } from 'ionic-angular';
 import { Component } from '@angular/core';
 import { Line } from '../../models/itinerary';
 import { FavoriteButton } from '../favorite-button/controller';
 
+/**
+ * Represents the <map-snackbar> HTML component.
+ * @class {MapSnackbar}
+ */
 @Component({
     selector: 'map-snackbar',
     templateUrl: 'build/components/map-snackbar/template.html',
@@ -19,6 +21,10 @@ export class MapSnackbar {
     public coming: string = 'SENTIDO';
     public going: string = 'DESCONHECIDO';
 
+    /**
+     * It's part of Angular2 lifecycle. It runs when the view is initialized.
+     * @return {void}
+     */
     public ngOnInit(): void {
         if (this.line && this.line.Description.indexOf(' X ') > -1) {
             let tmp: string[] = this.line.Description.split(' X ');
@@ -27,6 +33,10 @@ export class MapSnackbar {
         }
     }
 
+    /**
+     * Handles the event of click over swap direction button
+     * @return {void}
+     */
     public onToggleDirection(): void {
         let toggle: boolean = this.swapDirection(); // Method from MapPage
         if (toggle) {
