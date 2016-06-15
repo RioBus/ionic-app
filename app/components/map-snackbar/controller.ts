2f8aa05d89eba54a6044cc9b75a4da8a38f08c8f
Fred Souza <fredericoamsouza@gmail.com>
2016-06-14 22:21:23 -0300

    Fixing Snackbar behavior

--- app/components/map-snackbar/controller.ts
+++ app/components/map-snackbar/controller.ts
@@ -1,5 +1,5 @@
 import { Button } from 'ionic-angular';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Line } from '../../models/itinerary';
 import { FavoriteButton } from '../favorite-button/controller';
 import { BasePage } from '../../core/page';
@@ -14,32 +14,25 @@ import { BasePage } from '../../core/page';
     inputs: ['line', 'swapDirection', 'swapable'],
     directives: [Button, FavoriteButton],
 })
-export class MapSnackbar extends BasePage {
+export class MapSnackbar extends BasePage implements OnInit {
 
     public line: Line;
     public swapable: boolean;
     public swapDirection: () => boolean;
-    public coming: string = '';
-    public going: string = '';
+    public coming: string;
+    public going: string;
 
     public constructor() {
         super();
     }
 
-    public get Coming(): string {
-        return this.coming || this.Text.COMPONENT_SNACKBAR_COMING;
-    }
-
-    public get Going(): string {
-        return this.going || this.Text.COMPONENT_SNACKBAR_GOING;
-    }
-
     /**
-     * Part of Ionic lifecycle. Runs when the view is about to be presented.
+     * Part of Ionic lifecycle. Runs when the component is initialized.
      * @return {void}
      */
-    public ionViewWillEnter(): void {
+    public ngOnInit(): void {
         if (this.line && this.line.Description.indexOf(' X ') > -1) {
+            console.log(`Line: ${JSON.stringify(this.line)}`);
             let tmp: string[] = this.line.Description.split(' X ');
             this.coming = tmp[0];
             this.going = tmp[1];
