215d4bc10f07edb6bac84cce08c12f576016c2f6
Fred Souza <fredericoamsouza@gmail.com>
2016-06-14 21:20:45 -0300

    Documenting MapPage

--- app/pages/map/page.ts
+++ app/pages/map/page.ts
@@ -1,5 +1,3 @@
-'use strict';
-
 import { Component } from '@angular/core';
 import { NavParams } from 'ionic-angular';
 import { UPDATE_TIMEOUT } from '../../const';
@@ -9,6 +7,10 @@ import { Bus } from '../../models/bus';
 import { Line, Itinerary } from '../../models/itinerary';
 import { ItineraryManager } from '../../managers/itinerary';
 
+/**
+ * MapPage represents the view with the map presented right after the search view.
+ * @class {MapPage}
+ */
 @Component({
     templateUrl: 'build/pages/map/template.html',
     directives: [GoogleMapsComponent],
@@ -18,11 +20,11 @@ export class MapPage {
     private params: NavParams;
     private service: SearchService;
     private manager: ItineraryManager;
-    private title: string = 'Rio Bus';
     private timer: any;
     public buses: Bus[];
     public line: Line;
     public trajectory: Itinerary;
+    public title: string = '';
 
     public constructor(params: NavParams, service: SearchService, manager: ItineraryManager) {
         this.params = params;
@@ -38,21 +40,37 @@ export class MapPage {
         }
     }
 
-    public get Title(): string {
-        return this.title.replace(',', ', ');
-    }
-
+    /**
+     * Part of Ionic lifecycle. Runs when the view was just presented.
+     * @return {void}
+     */
     public ionViewLoaded(): void {
         this.showTrajectory();
         this.updateMarkers();
         this.timer = setInterval(() => this.updateMarkers(), UPDATE_TIMEOUT);
     }
 
+    /**
+     * Part of Ionic lifecycle. Runs when the view is about to be hidden.
+     * @return {void}
+     */
+    public ionViewWillLeave(): void {
+        clearInterval(this.timer);
+    }
+
+    /**
+     * Retrieves the trajectory of the searched line to present on the map.
+     * @return {void}
+     */
     private showTrajectory(): void {
         if (this.title.split(',').length === 1)
             this.manager.getByLine(this.title).then(itinerary => this.trajectory = itinerary);
     }
 
+    /**
+     * Update the markers on the map.
+     * @return {void}
+     */
     private updateMarkers(): void {
         this.service.getBuses(this.title).then((buses: Bus[]) => this.buses = buses, error => {
             this.buses = [];
@@ -60,9 +78,5 @@ export class MapPage {
             if (error.status === 404) console.log(`[404] No buses were found the the query '${this.title}'.`);
             else console.log(`[${error.status}] An error ocurred.`);
         });
-    }
-
-    public ionViewWillLeave(): void {
-        clearInterval(this.timer);
     }
 }
