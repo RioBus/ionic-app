5ae6e8d5dc6ebbed21c668aa4117ae22d52961fe
Fred Souza <fredericoamsouza@gmail.com>
2016-06-14 21:00:11 -0300

    Documenting AboutPage class

--- app/pages/about/page.ts
+++ app/pages/about/page.ts
@@ -1,6 +1,10 @@
 import { Component } from '@angular/core';
 import { BasePage } from '../../core/page';
 
+/**
+ * AboutPage represents the view with information about the app.
+ * @class {AboutPage}
+ */
 @Component({
     templateUrl: 'build/pages/about/template.html',
     directives: [],
