0ea4247d2ba75b17a025e4d7d8dc343806dbbcae
Fred Souza <fredericoamsouza@gmail.com>
2016-06-20 15:51:30 -0300

    Moving signing.sh to run.sh

--- signing/signing.sh
+++
@@ -1,7 +0,0 @@
-# Copying the debug signing properties to Android project root
-echo "Configuring Android signing properties...";
-cp "$(pwd)/config/debug-signing.properties" "$(pwd)/platforms/android";
-cp "$(pwd)/config/release-signing.properties" "$(pwd)/platforms/android";
-cp "$(pwd)/config/debug.keystore" "$(pwd)/platforms/android";
-cp "$(pwd)/config/release.keystore" "$(pwd)/platforms/android";
-echo "Finished.";
