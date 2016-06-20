d188d5debff76aba5bdf9360163a62c7e09002d7
Fred Souza <fredericoamsouza@gmail.com>
2016-06-20 16:06:41 -0300

    Updating signing scripts

--- signing/signing.sh
+++
@@ -1,8 +0,0 @@
-# Copying the debug signing properties to Android project root
-echo "Configuring Android signing properties...";
-ionic platform add android
-cp "$(pwd)/signing/debug-signing.properties" "$(pwd)/platforms/android";
-cp "$(pwd)/signing/release-signing.properties" "$(pwd)/platforms/android";
-cp "$(pwd)/signing/debug.keystore" "$(pwd)/platforms/android";
-# cp "$(pwd)/config/release.keystore" "$(pwd)/platforms/android";
-echo "Finished.";
