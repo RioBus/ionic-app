23ef84ebd84545190cfd8cd2b2e9cc34ad73c2d8
Fred Souza <fredericoamsouza@gmail.com>
2016-06-23 16:27:52 -0300

    Creating clear-history directive

---
+++ app/components/clear-history/controller.ts
@@ -0,0 +1,60 @@
+import { Directive, HostListener } from '@angular/core';
+import { Alert, NavController } from 'ionic-angular';
+import { HistoryDAO } from '../../dao/history';
+
+/**
+ * @export
+ * @class {ClearHistory}
+ * 
+ * Represents the 'clear-history' directive use to trigger the action to clear the history
+ * when the elements with this directive are clicked.
+ */
+@Directive({
+    selector: '[clear-history]',
+})
+export class ClearHistory {
+
+    private dao: HistoryDAO;
+    private nav: NavController;
+
+    public constructor(nav: NavController) {
+        this.nav = nav;
+        this.dao = new HistoryDAO();
+    }
+
+    /**
+     * Called when a HTML element with 'clear-history' directive is clicked. <br/>
+     * It opens an dialog which allows the user to clear his navigation history or cancel the operation.
+     * @return {void}
+     */
+    @HostListener('click', ['$event.target'])
+    public shouldClearHistory(): void {
+        let confirm: Alert = Alert.create({
+            title: 'Limpar Histórico',
+            message: 'Tem certeza que deseja apagar todo o histórico? Esta ação não pode ser desfeita.',
+            buttons: [
+                {
+                    text: 'Não',
+                    handler: (): void => console.log('Clear canceled'),
+                },
+                {
+                    text: 'Sim',
+                    handler: (): void => this.clearHistory(),
+                },
+            ],
+        });
+        this.nav.present(confirm);
+    }
+
+    /**
+     * Clears the history.
+     * @return {void}
+     */
+    private clearHistory(): void {
+        this.dao.clear()
+        .then(
+            () => console.log("Histórico apagado."),
+            error => console.log('Failed to clear history.')
+        );
+    }
+}
