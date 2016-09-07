16a035d138ce7ba2a5ab6c415efc6b652c410c0f
Fred Souza <fredericoamsouza@gmail.com>
2016-09-07 20:03:06 -0300

    Abstracting analytics plugin

---
+++ app/core/analytics.ts
@@ -0,0 +1,62 @@
+import { GoogleAnalytics } from 'ionic-native';
+import { ANALYTICS_UA } from '../const';
+
+/**
+ * Analytics driver API abstraction. 
+ * 
+ * @class Analytics
+ */
+export class Analytics {
+
+    /**
+     * Configures Analytics driver
+     * 
+     * @static
+     * @returns {Promise<void>}
+     */
+    public static configure(): Promise<void> {
+        return GoogleAnalytics.startTrackerWithId(ANALYTICS_UA)
+            .then(() => GoogleAnalytics.enableUncaughtExceptionReporting(true), console.error)
+            .then(() => GoogleAnalytics.debugMode(), console.error);
+    }
+
+    /**
+     * Track a screen
+     *
+     * @param {string}  title        - Screen title
+     * @param {string}  campaignUrl  - (Optional) Campaign url for measuring referrals
+     * 
+     * @static
+     * @returns {Promise<void>}
+     */
+    public static trackView(title: string, campaignUrl?: string): Promise<void> {
+        return GoogleAnalytics.trackView(title, campaignUrl)
+            .then(console.log, console.error);
+    }
+
+    /**
+     * Track an event
+     * @param {string}  category
+     * @param {string}  action
+     * @param {string}  label
+     * @param {number}  value
+     * 
+     * @static
+     * @returns {Promise<void>}
+     */
+    public static trackEvent(category: string, action: string, label?: string, value?: number): Promise<void> {
+        return GoogleAnalytics.trackEvent(category, action, label, value)
+            .then(console.log, console.error);
+    }
+
+    /**
+     * Set a UserId
+     * @param {string}  id
+     * 
+     * @static
+     * @returns {void}
+     */
+    public static setUserId(id: string): void {
+        GoogleAnalytics.setUserId(id);
+    }
+}
